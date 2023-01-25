e776cde07fb26f96005ab7819efbe5c666ad9cce
SzyWal <walachowski_szymon@wp.pl>
2023-01-25 19:28:08 +0100

    update js

--- Dokumenty/IT/VisualStudio/Project_1/snake.js
+++ Dokumenty/IT/VisualStudio/Project_1/snake.js
@@ -1,8 +1,10 @@
 window.addEventListener('load', () => {
-    let points, snake, running, apple, move, nextMove
-    const ctx = document.getElementById('snake-canvas').getContext('2d')
+
+    let snake, move, nextMove, points, apple, running;
+    const ctx = document.getElementById('snake-canvas').getContext('2d');
+
     setDefault();
-    addKeyDownEventListener()
+    addKeyDownEventListener();
     setInterval(renderFrame, 100);
 
     function renderFrame() {
@@ -11,47 +13,44 @@ window.addEventListener('load', () => {
                 move = nextMove;
             }
             snake.push({ x: processBound(getHead().x + move.x), y: processBound(getHead().y + move.y) });
-            if (snake.filter(square => square.x === getHead() && square.y === getHead()).length >= 2) {
+            if (snake.filter(square => square.x === getHead().x && square.y === getHead().y).length >= 2) {
                 setDefault();
             } else {
-
-                if (apple.x === getHead() && apple.y === getHead().y) {
+                console.log(getHead());
+                if (getHead().x === apple.x && getHead().y === apple.y) {
                     points++;
                     apple = generateAppleLocation();
                 }
-
                 points <= 0 ? snake.shift() : points--;
             }
-
         }
+
         ctx.fillStyle = 'black';
         ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-
         ctx.fillStyle = 'yellow';
         snake.forEach(square => ctx.fillRect(square.x * 20, square.y * 20, 18, 18));
-
         ctx.fillStyle = 'red';
-        ctx.fillRect(apple.x * 20, apple.y * 20, 18, 18);
+        ctx.fillRect(apple.x * 20, apple.y * 20, 18, 18)
     }
 
     function getHead() {
         return snake[snake.length - 1];
     }
 
-    function processBound(number) {
-        if (number > 19) {
+    function processBound(xOrY) {
+        if (xOrY > 19) {
             return 0;
-        } else if (number < 19) {
+        } else if (xOrY < 0) {
             return 19;
         }
-        return number;
+        return xOrY;
     }
 
     function setDefault() {
-        running = false;
-        points = 2;
-        [move, nextMove] = Array(2).fill({ x: 0, y: 0 });
         snake = [{ x: 10, y: 10 }];
+        [move, nextMove] = Array(2).fill({ x: 0, y: 0 });
+        points = 2;
+        running = false;
         apple = generateAppleLocation();
     }
 
@@ -59,7 +58,7 @@ window.addEventListener('load', () => {
         let location;
         do {
             location = { x: generateRandomNumber(19), y: generateRandomNumber(19) };
-        } while (snake.filter(square => square.x === location.x && square.y).length > 0);
+        } while (snake.filter(square => square.x === location.x && square.y === location.y).length > 0);
         return location;
     }
 
@@ -74,16 +73,16 @@ window.addEventListener('load', () => {
                 running = true;
             }
             switch (e.code) {
-                case "ArrowLeft":
+                case 'ArrowLeft':
                     nextMove = { x: -1, y: 0 };
                     break;
-                case "ArrowRight":
+                case 'ArrowRight':
                     nextMove = { x: 1, y: 0 };
                     break;
-                case "ArrowDown":
+                case 'ArrowDown':
                     nextMove = { x: 0, y: 1 };
                     break;
-                case "ArrowUp":
+                case 'ArrowUp':
                     nextMove = { x: 0, y: -1 };
             }
         });
