719967ff238dfc0a8003688f234711af74ddb042
SzyWal <walachowski_szymon@wp.pl>
2023-02-19 20:42:44 +0100

    fixed some errors

--- Dokumenty/IT/VisualStudio/qr-code-component-main/app.js
+++ Dokumenty/IT/VisualStudio/qr-code-component-main/app.js
@@ -1,1 +1,6 @@
-aaa
+function getAge(inputString){
+  // return the girl's correct age as an integer. Happy coding :) 
+  let a = inputString.slice('')
+    return Math.floor(Math.random(a[0]) * (9 - 0) + 0)
+    cosnole.log(a.slice(inputString))
+  }
